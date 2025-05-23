ae6a8e13255240158c0fbceac536584a6fbad616
Erik <eriklenda1@gmail.com>
2025-05-23 14:23:06 -0300

    aula 23/05/ props

--- src/App.jsx
+++ src/App.jsx
@@ -2,21 +2,26 @@ import "./styles/theme.css";
 import "./styles/global.css";
 import "./app.css";
 import { MyText } from "./components/Mytext";
+
 export default function App() {
+  const texts = [
+    { title: "React Component 1", text: "Olá mundo" },
+    { title: "React Component 2", text: "Olá mundo" },
+    { title: "React Component 3", text: "Olá mundo" },
+    { title: "React Component 4", text: "Olá mundo" },
+    { title: "React Component 5", text: "Olá mundo" },
+  ];
+
   return (
-    <div>
+    //react fragment
+    <>
       <h1>Welcome to the React App</h1>
       <p>This is a simple React application.</p>
-    </div>
-  );
-}
-export default function App() {
-  return (
-    //E-REACT fragment
-    <> 
-    <MyText />
-    <MyText />
-    <MyText />
+      {texts.map((text, index) => (
+        <MyText key={index} title={`${text.title} ${index + 1}`}>
+          {text.text}
+        </MyText>
+      ))}
     </>
   );
 }
--- src/components/MyText.jsx
+++ src/components/MyText.jsx
@@ -1,15 +1,12 @@
 import styles from './MyText.module.css';
 
-export function MyText() {
+export function MyText({title, children}) {
   return(
     <div className={styles.container}>
-     <div className={styles.div}>
-  <h1 className={styles.tittle} > Meu primeiro react</h1>
-    <p className={styles.text}> Meu primeiro react</p>  
-  (
-    <div>
-      <h1>My Text Component</h1>
-      <p>This is a simple text component.</p>
+      <div className={styles.div}>
+        <h1 className={styles.title}>{title}</h1>
+        <p className={styles.text}>{children}</p>
+      </div>
     </div>
   );
 }
